3d4919403ecb2dcd804b4ebc7d4ead919053ea3c
comba, yannick <y.comba@maine-et-loire.fr>
2023-09-07 16:29:29 +0200

    Ajout commentaire JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Check if the form is there
 if (document.querySelector('.tt_entity_choice') != null) {
     document.querySelector('#tt_gest_error').style.display='none'
     document.querySelector('.form_transfert').style.display='block'
@@ -9,19 +10,22 @@ if (document.querySelector('.tt_entity_choice') != null) {
     const clone_all_groups = document.querySelectorAll('#group_choice option')
     let all_groups = []
 
+    // Remove all groups not chosen
     let all_groups_unchoice = document.querySelectorAll('#group_choice option')
     all_groups_unchoice.forEach(function(all_group_unchoice) {
         all_group_unchoice.remove()
     })
 
     entity_choice.addEventListener('click', function (event) {
-        if(entity_choice.value == '') {
+        // if value is empty, hide groups
+        if (entity_choice.value == '') {
             tt_group_choice.style.display = 'none'
             tt_btn_open_modal_form.disabled = true
             tt_btn_open_modal_form.style.backgroundColor = '#D3D3D3'
             tt_btn_open_modal_form.style.color = '#FFFFFF'
             tt_btn_open_modal_form.style.cursor = 'not-allowed'
         } else {
+        // if not, show them
             tt_group_choice.style.display = 'block'
             document.querySelector('#div_confirmation').style.display = 'block'
             document.querySelector('.tt_group_choice').style.display = 'block'
@@ -33,23 +37,28 @@ if (document.querySelector('.tt_entity_choice') != null) {
         all_groups = clone_all_groups
 
         all_groups.forEach(function(all_group) {
-            if('tt_plugin_entity_' + entity_choice.value == all_group.className || all_group.value == '') {
+            // Add groups of selected entity
+            if ('tt_plugin_entity_' + entity_choice.value == all_group.className || all_group.value == '') {
                 document.querySelector('#group_choice').appendChild(all_group)
             } else {
+            // Delete previous groups
                 all_group.remove()
             }
         })
 
+        // if another entity is chosen, reset the selected group
         document.querySelector('#no_select').selected = true
     })
 
     document.querySelector('.form_transfert').addEventListener('click', function (event) {
-        if(document.querySelector('#group_choice').value == '') {
+        // if no group selected, disabled the confirm button
+        if (document.querySelector('#group_choice').value == '') {
             tt_btn_open_modal_form.disabled = true
             tt_btn_open_modal_form.style.backgroundColor = '#D3D3D3'
             tt_btn_open_modal_form.style.color = '#FFFFFF'
             tt_btn_open_modal_form.style.cursor = 'not-allowed'
         } else {
+        // else, enable it
             tt_btn_open_modal_form.disabled = false
             tt_btn_open_modal_form.style.backgroundColor = '#80cead'
             tt_btn_open_modal_form.style.color = '#1e293b'
@@ -59,11 +68,13 @@ if (document.querySelector('.tt_entity_choice') != null) {
 
     let modal_form_adder = document.getElementById('tt_modal_form_adder')
 
+    // Open modal
     document.querySelector('#canceltransfert').addEventListener('click', function(event){
         event.preventDefault()
         modal_form_adder.close();
     });
 
+    // Close modal
     tt_btn_open_modal_form.addEventListener('click', function(event){
         event.preventDefault()
         modal_form_adder.showModal();
